8120b3ce6670002be6a611c95c888c974ebedf32
StarGale <zhenchix@hotmail.com>
2019-06-16 12:09:43 -0700

    basic layout

---
+++ portfolio/src/components/articles/articles.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import "./Articles.css";
+
+class Articles extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div style={{ width: "85vw", margin: "3em auto" }}>
+        <div className="row flex-center">
+          <div
+            className="card sm-12 md-7 col"
+            style={{ width: "40rem", margin: "3em auto" }}
+          >
+            <div className="card-header">What's New</div>
+            <img src="https://picsum.photos/500" alt="Card example image" />
+
+            <div className="card-body">
+              <h4 className="card-title">My awesome Paper card!</h4>
+              <h5 className="card-subtitle">Nice looking subtitle.</h5>
+              <p className="card-text">
+                Notice that the card width in this example have been set to
+                20rem, otherwise it will try to fill the current container/row
+                where the card is.
+              </p>
+              <wired-card elevation="4">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Natus qui mollitia voluptatibus aut aspernatur quidem
+                  repudiandae, voluptate sint itaque explicabo?
+                </p>
+              </wired-card>
+              <hr />
+              <button>Visit the site</button>
+            </div>
+          </div>
+
+          <div
+            className="card sm-12 md-4 col"
+            style={{
+              // width: "5rem",
+              margin: "2em auto"
+            }}
+          >
+            <wired-card class="API-news" elevation="3">
+              <div className="card-body">
+                <h4 className="card-title">My awesome Paper card!</h4>
+                <h5 className="card-subtitle">Nice looking subtitle.</h5>
+                <p className="card-text">
+                  This is another example of a card without image. Cards are
+                  also meant to be used without images, but with
+                  text/links/buttons.
+                </p>
+                <a className="card-link" href="#">
+                  First link
+                </a>
+                <a className="card-link" href="#">
+                  Second link
+                </a>
+              </div>
+            </wired-card>
+            <wired-card class="API-news" elevation="3">
+              <div className="card-body">
+                <h4 className="card-title">My awesome Paper card!</h4>
+                <h5 className="card-subtitle">Nice looking subtitle.</h5>
+                <p className="card-text">
+                  This is another example of a card without image. Cards are
+                  also meant to be used without images, but with
+                  text/links/buttons.
+                </p>
+                <a className="card-link" href="#">
+                  First link
+                </a>
+                <a className="card-link" href="#">
+                  Second link
+                </a>
+              </div>
+            </wired-card>
+          </div>
+        </div>
+        <div className="row">
+          <div class="col-4 col">
+            <div className="card">
+              <p className="card-text">col-4 col</p>
+            </div>
+          </div>
+
+          <div class="col-4 col">
+            <div className="card">
+              <p className="card-text">col-4 col</p>
+            </div>
+          </div>
+
+          <div class="col-4 col">
+            <div className="card">
+              <p className="card-text">col-4 col</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Articles;
--- portfolio/src/components/navbar/navbar.js
+++ portfolio/src/components/navbar/navbar.js
@@ -1,43 +1,59 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
+import Portfolios from "../portfolios/portfolios";
+import Articles from "../articles/articles"
+
+const Index = () => {
+  return <h2>Home</h2>;
+};
+
+
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   render() {
     return (
-      <div>
-        <p>show this message</p>
-        <nav className="border fixed split-nav">
-          <div className="nav-brand">
-            <h3>CHI'S PORTFOLIOS</h3>
-          </div>
-          <div className="collapsible">
-            <input id="collapsible1" type="checkbox" name="collapsible1" />
-            <button>
-              <label for="collapsible1">
-                <div className="bar1" />
-                <div className="bar2" />
-                <div className="bar3" />
-              </label>
-            </button>
-            <div className="collapsible-body">
-              <ul className="inline">
-                <li>
-                  <a href="#">Capabilities</a>
-                </li>
-                <li>
-                  <a href="#">Works</a>
-                </li>
-                <li>
-                  <a href="#">What's New</a>
-                </li>
-              </ul>
+      <Router>
+        <div>
+          <p>show this message</p>
+          <nav className="border fixed split-nav">
+            <div className="nav-brand">
+              <h3>CHI'S PORTFOLIOS</h3>
             </div>
-          </div>
-        </nav>
-      </div>
+            <div className="collapsible">
+              <input id="collapsible1" type="checkbox" name="collapsible1" />
+              <button>
+                <label for="collapsible1">
+                  <div className="bar1" />
+                  <div className="bar2" />
+                  <div className="bar3" />
+                </label>
+              </button>
+              <div className="collapsible-body">
+                <ul className="inline">
+                  <li>
+                    <Link to="/">Capabilities</Link>
+                  </li>
+                  <li>
+                    <Link to="/works">Works</Link>
+                  </li>
+                  <li>
+                    <Link to="articles">What's New</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <Route path="/" exact component={Index} />
+        <Route path="/works/" component={Portfolios} />
+        <Route path="/articles/" component={Articles} />
+      </Router>
     );
   }
 }
